dd142f4e4207ec94ec9498adabcf98dc0e781433
Parna-tech32 <santrasaptaparna@gmail.com>
2026-04-15 22:30:30 +0530

    Refactor PLOT.py to improve code clarity

--- PLOT.py
+++ PLOT.py
@@ -1,18 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Load data
 data = np.load("data/test/P-1.npy")
 
-# Take only one column
 data = data[:, 0]
-
-# Known anomaly region
 start, end = 2149, 2349
 
-# Plot
+
 plt.plot(data)
-plt.axvspan(start, end, alpha=0.3)  # highlight anomaly
+plt.axvspan(start, end, alpha=0.3)  # anomaly
 
 plt.title("Telemetry Data with Anomaly Region")
 plt.xlabel("Time Index")
